76b28974edcc8589802baed9fbd58e2acb56c242
Tim Boudreaux <tmboudreaux00@gmail.com>
2020-10-10 18:21:37 -0500

    Path and framework set up

---
+++ src/test/java/com/blog/jt/JtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.jt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
