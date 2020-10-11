421fa3929c5691d7303ba3869b59a438f6b5146f
Tim Boudreaux <tmboudreaux00@gmail.com>
2020-10-10 20:12:34 -0500

    added logic to PostController

--- src/main/java/com/blog/jt/controllers/PostController.java
+++ src/main/java/com/blog/jt/controllers/PostController.java
@@ -1,4 +1,81 @@
 package com.blog.jt.controllers;
 
+import com.blog.jt.models.Post;
+import com.blog.jt.models.User;
+import com.blog.jt.repositories.PostRepository;
+import com.blog.jt.repositories.UserRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
 public class PostController {
+    private final PostRepository postRepository;
+    private final UserRepository userRepository;
+
+    public PostController(PostRepository postRepository, UserRepository userRepository) {
+        this.postRepository = postRepository;
+        this.userRepository = userRepository;
+    }
+
+    @GetMapping("/post")
+    public String showPosts(Model model) {
+        List<Post> posts = postRepository.findAll();
+        model.addAttribute("posts", posts);
+        return "posts/index";
+    }
+
+    @GetMapping("/posts/{id}")
+    public String showPost(@PathVariable Integer id, Model model) {
+        Post post = postRepository.getPostById(id);
+        model.addAttribute("post", post);
+        return "posts/show";
+    }
+
+    @GetMapping("/posts/create")
+    public String showCreatePost () {
+        return "posts/create";
+    }
+
+    @PostMapping("/posts/create")
+    public String createPost(@RequestParam(name = "title") String title,
+                             @RequestParam(name = "body") String body,
+                             Model model) {
+        List<User> users = userRepository.findAll();
+        Post post = new Post();
+        post.setTitle(title);
+        post.setBody(body);
+        if (!users.isEmpty()) post.setOwner(users.get(0));
+        postRepository.save(post);
+        return "redirect:/posts/" + post.getId();
+    }
+
+    @GetMapping("/posts/delete/{id}")
+    public String deleteAd(@PathVariable long id, Model model) {
+        Post post = postRepository.getPostById(id);
+        postRepository.delete(post);
+        return "redirect:/posts";
+    }
+
+    @GetMapping("/posts/edit/{id}")
+    public String editPost(@PathVariable long id, Model model) {
+        Post post = postRepository.getPostById(id);
+        model.addAttribute("post", post);
+        return "posts/edit";
+    }
+
+    @PostMapping("/posts/edit")
+    public String updateAd(@RequestParam(name = "id") long id,
+                           @RequestParam(name = "title") String title,
+                           @RequestParam(name = "body") String body) {
+        Post post = new Post();
+        post.setId(id);
+        post.setTitle(title);
+        post.setBody(body);
+        postRepository.save(post);
+        return "redirect:/posts/" + post.getId();
+    }
 }
