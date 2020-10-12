5ad5e278e5c61f8d2b0748b8ed9fe501466daaf8
Tim Boudreaux <tmboudreaux00@gmail.com>
2020-10-12 13:06:54 -0500

    work on home controller

--- src/main/java/com/blog/jt/controllers/HomeController.java
+++ src/main/java/com/blog/jt/controllers/HomeController.java
@@ -4,16 +4,14 @@ package com.blog.jt.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Random;
-
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    @ResponseBody
     public String hello() {
-        return "This is the landing page!";
+
+        return "home";
     }
 
 }
